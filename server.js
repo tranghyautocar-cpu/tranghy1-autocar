64053a30b6dfb32331269164bc815f80cd55c942
tranghyautocar-cpu <tranghyautocar@gmail.com>
2026-01-08 00:36:22 +0700

    xong nhe

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const db = require('./database/db'); 
+const db = require('./backend/database/db'); 
 
 const app = express();
 const PORT = 5000;
